2d67afc438c8b0b67d862f8cf866fe7c6bec29f1
Audrow Nash <114802901+paudrow@users.noreply.github.com>
2024-06-23 02:11:04 +0000

    Set to podcast website

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('*', (req, res) => {
-  res.redirect('https://linktr.ee/audrow');
+  res.redirect('https://www.audrownashpodcast.com/');
 });
 
 const port = process.env.PORT || 3000;
