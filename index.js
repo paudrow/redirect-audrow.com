264e3be1cbcdf063265d5d3ae8077a63dd9bb227
Audrow Nash <114802901+paudrow@users.noreply.github.com>
2024-07-28 08:03:29 -0500

    Redirect to audrownashpodcast.com

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('*', (req, res) => {
-  res.redirect('https://audrow-nash-podcast.ghost.io/');
+  res.redirect('https://audrownashpodcast.com/');
 });
 
 const port = process.env.PORT || 3000;
